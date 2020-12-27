b65ed7455a18675a7086df9fb673ec617104b754
Gábor Csomor <gaborcsomorstats@gmail.com>
2020-12-27 19:31:43 +0100

    Add files via upload

--- 03_LCA.R
+++ 03_LCA.R
@@ -1,7 +1,6 @@
-setwd("C:/Users/Gabor/Documents/00_Vallalkozas/02_PPK/adatok/02_analysis")
-library(haven)
-
 # import clean databases
+
+setwd("C:/Users/Gabor/Documents/00_Vallalkozas/02_PPK/adatok/02_analysis")
 
 df_k = read_sav("kontrol_imp.sav")
 df_p = read_sav("pozitiv_imp.sav")
@@ -11,160 +10,343 @@ df = read_sav("dat_imputation_final_cimkekkel.sav") # 3 in 1
 setwd("C:/Users/Gabor/Documents/00_Vallalkozas/02_PPK/adatok/02_analysis/01_LCA")
 
 
+# import packages 
 
+library(haven)
+library(ggplot2)
+library(poLCA) 
 
-################################
-# LCA -- nemzeti identifikáció #
-################################
-
-library("poLCA") 
 # NB: PCA with poLCA treats all variables 
 # as qualitative/categorical/nominal – NOT as ordinal.
 
-i = which(names(df_k)== "nemz_id_1")
-j = which(names(df_k)== "nemz_id_8")
 
-df_k_nemz = df_k[i:j]
 
-formula = cbind(nemz_id_1, nemz_id_2, nemz_id_3, nemz_id_4, nemz_id_5, nemz_id_6, nemz_id_7, nemz_id_8) ~ 1
 
-set.seed(1)
-fitlca1 <- poLCA::poLCA(formula, data = df_k_nemz, nclass = 1,
-                        nrep = 3, maxiter = 5000, verbose = FALSE)
-fitlca2 <- poLCA::poLCA(formula, data = df_k_nemz, nclass = 2,
-                 nrep = 3, maxiter = 5000, verbose = FALSE)
-fitlca3 <- poLCA::poLCA(formula, data = df_k_nemz, nclass = 3,
-                        nrep = 3, maxiter = 5000, verbose = FALSE)
-fitlca4 <- poLCA::poLCA(formula, data = df_k_nemz, nclass = 4,
-                        nrep = 3, maxiter = 5000, verbose = FALSE)
-fitlca5 <- poLCA::poLCA(formula, data = df_k_nemz, nclass = 5,
-                        nrep = 3, maxiter = 5000, verbose = FALSE)
+##########################
+# fuggvenyek létrehozása #
+##########################
 
-# fitlca1$bic;fitlca2$bic;fitlca3$bic;fitlca4$bic;fitlca5$bic;
-# fitlca2$npar
-# fitlca2$P
 
-# abrazoljuk a modell illeszkedest
+# 1. BIC ábra 
 
-b_temp = cbind(fitlca1$bic, fitlca2$bic, fitlca3$bic, 
-          fitlca4$bic, fitlca5$bic)
-b_t = as.data.frame(t(b_temp))
-names(b_t) = "BIC"
-b <- cbind(C = rownames(b_t), b_t, row.names=NULL)
-# b
-
-library(ggplot2)
-
-png("Nemz_id_BIC.png", width = 500, height = 300, res=80)
-ggplot(data = b, 
-  aes(x = C, y = BIC, group = 1)) +
-  geom_point(size = 2) +
-  geom_line() +
+bic_abra = function(lcalist = lcalist, n = 5,
+                    fajlnev = "Sztip_men_BIC.png",
+                    abracim = "Sztereotípia menekültekkel szemben"){
+  b_temp = 0
+  for (j in 1:n){
+    b_temp[j] = cbind(round(lcalist[[j]]$bic,0)) # extract BIC values
+    names(b_temp)[j] = names(lcalist)[j]
+  }
   
-  coord_cartesian( ylim = c(0, max(b_temp))) +
-  xlab ("Látens osztályok száma") +
+  iter = rep(1:n)
+  b_temp2 = rbind(BIC = b_temp, C = iter) 
+  b_t = as.data.frame(t(b_temp2)) # store BIC in a df
+  b = cbind(b_t, model = rownames(b_t))
+  rownames(b) <- NULL
   
-  ggtitle( "Nemzeti identifikáció") +
-  theme(plot.title = element_text(hjust = 0.5)) 
+  # create plot
+  png(fajlnev, width = 500, height = 300, res=80) # !
+  abra_BIC = ggplot2::ggplot(data = b, 
+                         aes(x = C, y = BIC, group = 1, label = BIC)) +
+    geom_point(size = 2) +
+    geom_line() +
+    geom_text(size = 3.5, position = position_stack(vjust = 0.8)) + # value labels on chart
+    
+    coord_cartesian( ylim = c(0, max(b_temp))) +
+    xlab ("Látens osztályok száma") +
+    
+    ggtitle(abracim) +  # !
+    theme(plot.title = element_text(hjust = 0.5)) 
   # ajánlott multilpot fő cím: "LCA modell illeszkedése az adatokra"
-dev.off()
-
-
-# eloszlasvizsgalat
-
-library(purrr)
-library(tidyr)
-library(ggplot2)
-
-df_k_nemz %>%
-  keep(is.numeric) %>% 
-  gather() %>% 
-  ggplot(aes(value)) +
-  facet_wrap(~ key, scales = "free") +
-  geom_bar() + 
-  xlab("Válasz") + ylab("Létszám") 
-
-# 1,3,8 nagyon nem normalis eloszlasu!
-# ezt csak akkor hasznalhatjuk, ha ide factor scorokat dugunk be.
-
-
-
-# abrazoljuk a manifest valtozok atlagat clusterenkent
-
-# plot(fitlca2)
-
-clusmemb <- as.factor(fitlca2$predclass) # predikcio
-# fitlca2$posterior # itt vannak a valoszinusegek
-
-df_k_nemz_c1 = df_k_nemz[clusmemb==1,]
-df_k_nemz_c2 = df_k_nemz[clusmemb==2,]
-
-df_k_nemz_m1 = apply(df_k_nemz_c1, 2, mean)
-df_k_nemz_m2 = apply(df_k_nemz_c2, 2, mean)
-
-
-# base R plot
-
-plot(df_k_nemz_m1, type = "b", ylim = c(1,5), col = "#0C7BDC", 
-     pch = 15, cex.main = 1,
-     main = "Nemzeti identifikáció \nkérdésekre adott válaszok átlaga látens osztályonként ", ylab = "válasz", xlab = "kérdés")
-lines(df_k_nemz_m2, type = "b", col = "#FFC20A", pch = 19)
-legend(1,2, legend=c("egészséges nemzeti id.", "esszencialista"),
-       col=c("#0C7BDC", "#FFC20A"), pch = c(15,19), cex = 0.8,
-       box.col = "white")
-
-
-# ggplot 
-
-# Basic line plot with points
-
-df_k_nemz_m1_temp = data.frame(as.list(df_k_nemz_m1))
-df_k_nemz_m1_temp2 = as.data.frame(t(df_k_nemz_m1_temp))
-# df_k_nemz_m1_temp2$Q = rownames(df_k_nemz_m1_temp2)
-df_k_nemz_m1_temp3 <- cbind(Kérdés = rownames(df_k_nemz_m1_temp2), data.frame(df_k_nemz_m1_temp2, row.names=NULL), Osztály = rep("egészséges nemzeti id.", 8))
-# df_k_nemz_m1_temp3
-
-df_k_nemz_m2_temp = data.frame(as.list(df_k_nemz_m2))
-df_k_nemz_m2_temp2 = as.data.frame(t(df_k_nemz_m2_temp))
-# df_k_nemz_m1_temp2$Q = rownames(df_k_nemz_m1_temp2)
-df_k_nemz_m2_temp3 <- cbind(Kérdés = rownames(df_k_nemz_m2_temp2), data.frame(df_k_nemz_m2_temp2, row.names=NULL), Osztály = rep("esszencialista", 8))
-# df_k_nemz_m2_temp3
-
-df_k_nemz_df = rbind(df_k_nemz_m1_temp3, df_k_nemz_m2_temp3)
-names(df_k_nemz_df)[names(df_k_nemz_df)=="V1"] = "Válasz"
-# class(df_k_nemz_df)
-
-
-
-
-# cimkek gyartasa aranyszamokkal
-
-arany = round(fitlca2$P,2)
-cimke = c("egészséges nemzeti id.", "esszencialista") # ezt automtizaljuk
-L = t(cbind(c1 = arany, cimke))
-# L
-
-c1 = paste0(L[2,1], " (", L[1,1], ")")
-c2 = paste0(L[2,2], " (", L[1,2], ")")
-# c1;c2
-
-png("Nemz_id_atlag.png", width = 500, height = 300, res=80)
-ggplot(data = df_k_nemz_df, 
-       aes(x = Kérdés, y = Válasz,
-           group = Osztály)) +
   
-  geom_line(aes(color = Osztály), size=1) +
-  geom_point(aes(color = Osztály, shape = Osztály), size=3) +
-  scale_color_manual(values=c("#0C7BDC", "#FFC20A"), labels = c(c1, c2)) +
-  scale_shape_manual(values=c(15, 19), labels = c(c1, c2)) +
+  print(abra_BIC)
+  dev.off()
   
-  theme(legend.position="bottom") +
-  coord_cartesian( ylim = c(1, 5))
-  
-  #ggtitle( "Nemzeti identifikáció \nkérdésekre adott válaszok átlaga látens osztályonként") +
-  #theme(plot.title = element_text(hjust = 0.5)) 
-  #+ xlab("Kérdés") + ylab("Válasz") 
-  # ezek ele jo lenne egy line break. NB: \n nem mukodik.
-dev.off()
+  return(abra_BIC)
+}
 
 
+# 2. válaszátlagok ábra
+
+valasz_abra = function(lcalist = lcalist, k = 2,
+                       fajlnev = "Sztip_men.png"){
+  # !
+  clusmemb <- as.factor(lcalist[[k]]$predclass) # itt csinaljunk egy elagazast k = 3-ra
+  
+  manif_c1 = manif[clusmemb==1,]
+  manif_c2 = manif[clusmemb==2,]
+  
+  manif_m1 = apply(manif_c1, 2, mean) # set up df for ggplot
+  manif_m2 = apply(manif_c2, 2, mean)
+  
+  manif_m1_temp = data.frame(as.list(manif_m1))
+  manif_m1_temp2 = as.data.frame(t(manif_m1_temp))
+  manif_m1_temp3 <- cbind(Kérdés = rownames(manif_m1_temp2), data.frame(manif_m1_temp2, row.names=NULL), Osztály = rep("c1", nrow(manif_m1_temp2)))
+  
+  manif_m2_temp = data.frame(as.list(manif_m2))
+  manif_m2_temp2 = as.data.frame(t(manif_m2_temp))
+  manif_m2_temp3 <- cbind(Kérdés = rownames(manif_m2_temp2), data.frame(manif_m2_temp2, row.names=NULL), Osztály = rep("c2", nrow(manif_m2_temp2)))
+  
+  manif_avg = rbind(manif_m1_temp3, manif_m2_temp3)
+  names(manif_avg)[names(manif_avg)=="V1"] = "Válasz"
+  
+  # !
+  arany = round(lcalist[[k]]$P,2) # cimkeerteke gyartasa jelmagyarazathoz
+  cimke = c("c1", "c2") # !
+  L = t(cbind(c1 = arany, cimke))
+  
+  c1 = paste0(L[2,1], " (", L[1,1], ")")
+  c2 = paste0(L[2,2], " (", L[1,2], ")")
+  
+  # ensure categories are not reordered on x axis
+  manif_avg$Kérdés <- factor(manif_avg$Kérdés, levels = unique(manif_avg$Kérdés)) 
+  
+  # !
+  png(fajlnev, width = 500, height = 300, res=80) # !
+  abra_atlag = ggplot2::ggplot(data = manif_avg, 
+                               aes(x = Kérdés, y = Válasz,
+                                   group = Osztály)) +
+    
+    geom_line(aes(color = Osztály), size=1) +
+    geom_point(aes(color = Osztály, shape = Osztály), size=3) +
+    scale_color_manual(values=c("#0C7BDC", "#FFC20A"), labels = c(c1, c2)) +
+    scale_shape_manual(values=c(15, 19), labels = c(c1, c2)) +
+    
+    theme(legend.position="bottom", axis.text.x = element_text(angle = 45, hjust = 1)) +
+    coord_cartesian( ylim = c(1, 5))
+  
+  print(abra_atlag)
+  dev.off()
+  return(abra_atlag)
+}
+
+
+
+
+
+####################################
+# LCA -- sztereotípia: menekültek #
+####################################
+
+# 1. define PCA formula
+
+# names(df_k)
+
+# !
+i = which(names(df_k)== "sztip_men_1")
+j = which(names(df_k)== "sztip_men_15")
+
+manif = df_k[i:j]
+
+s = names(manif)
+s2 = paste(s, collapse=" ")
+s3 = gsub(" ", ", ", s2)
+s4 = noquote(s3)
+# s4
+
+# !
+formula = cbind(sztip_men_1, sztip_men_2, sztip_men_3, sztip_men_4, sztip_men_5, sztip_men_6, sztip_men_7, sztip_men_8, sztip_men_9, sztip_men_10, sztip_men_11, sztip_men_12, sztip_men_13, sztip_men_14, sztip_men_15
+                ) ~ 1
+
+
+# 2. fit LCA model
+
+# !
+n = 5 # define the number of maximum clusters the model should try
+lcalist = list()
+
+for (i in 1:n){
+  
+  set.seed(1)
+  fitlca = poLCA::poLCA(formula, data = manif, nclass = i,
+               nrep = 3, maxiter = 5000, verbose = FALSE)
+  
+  name <- paste('fitlca',i,sep='')
+  lcalist[[name]] <- fitlca
+}
+
+# names(lcalist)
+# lcalist[[3]]
+
+
+
+
+# 3. abrazoljuk a modellek illeszkedeset
+
+bic_abra(lcalist = lcalist, n = n, 
+         fajlnev = "BIC_sztip_men.png",
+         abracim = "Sztereotípia menekültekkel szemben")
+
+
+
+
+# 4. abrazoljuk a manifest valtozok atlagat clusterenkent
+
+valasz_abra(lcalist = lcalist, k = 2, fajlnev = "Atlag_sztip_men.png")
+
+
+
+
+
+####################################
+# LCA -- sztereotípia: bevándorlók #
+####################################
+
+# 1. define PCA formula
+
+# names(df_k)
+
+# !
+i = which(names(df_k)== "sztip_bev_1")
+j = which(names(df_k)== "sztip_bev_15")
+
+manif = df_k[i:j]
+
+s = names(manif)
+s2 = paste(s, collapse=" ")
+s3 = gsub(" ", ", ", s2)
+s4 = noquote(s3)
+# s4
+
+# !
+formula = cbind(sztip_bev_1, sztip_bev_2, sztip_bev_3, sztip_bev_4, sztip_bev_5, sztip_bev_6, sztip_bev_7, sztip_bev_8, sztip_bev_9, sztip_bev_10, sztip_bev_11, sztip_bev_12, sztip_bev_13, sztip_bev_14, sztip_bev_15
+) ~ 1
+
+
+# 2. fit LCA model
+
+# !
+n = 5 # define the number of maximum clusters the model should try
+lcalist = list()
+
+for (i in 1:n){
+  
+  set.seed(1)
+  fitlca = poLCA::poLCA(formula, data = manif, nclass = i,
+                        nrep = 3, maxiter = 5000, verbose = FALSE)
+  
+  name <- paste('fitlca',i,sep='')
+  lcalist[[name]] <- fitlca
+}
+
+
+# 3. abrazoljuk a modellek illeszkedeset
+
+bic_abra(lcalist = lcalist, n = n, 
+         fajlnev = "BIC_sztip_bev.png",
+         abracim = "Sztereotípia bevándorlókkal szemben")
+
+
+# 4. abrazoljuk a manifest valtozok atlagat clusterenkent
+
+valasz_abra(lcalist = lcalist, k = 2, fajlnev = "Atlag_sztip_bev.png")
+
+
+
+##############################################
+# LCA -- sztereotípia: külföldi egyetemisták #
+##############################################
+
+# 1. define PCA formula
+
+# names(df_k)
+
+# !
+i = which(names(df_k)== "sztip_kEgy_1")
+j = which(names(df_k)== "sztip_kEgy_15")
+
+manif = df_k[i:j]
+
+s = names(manif)
+s2 = paste(s, collapse=" ")
+s3 = gsub(" ", ", ", s2)
+s4 = noquote(s3)
+# s4
+
+# !
+formula = cbind(sztip_kEgy_1, sztip_kEgy_2, sztip_kEgy_3, sztip_kEgy_4, sztip_kEgy_5, sztip_kEgy_6, sztip_kEgy_7, sztip_kEgy_8, sztip_kEgy_9, sztip_kEgy_10, sztip_kEgy_11, sztip_kEgy_12, sztip_kEgy_13, sztip_kEgy_14, sztip_kEgy_15
+) ~ 1
+
+
+# 2. fit LCA model
+
+# !
+n = 5 # define the number of maximum clusters the model should try
+lcalist = list()
+
+for (i in 1:n){
+  
+  set.seed(1)
+  fitlca = poLCA::poLCA(formula, data = manif, nclass = i,
+                        nrep = 3, maxiter = 5000, verbose = FALSE)
+  
+  name <- paste('fitlca',i,sep='')
+  lcalist[[name]] <- fitlca
+}
+
+
+# 3. abrazoljuk a modellek illeszkedeset
+
+bic_abra(lcalist = lcalist, n = n, 
+         fajlnev = "BIC_sztip_kEgy.png",
+         abracim = "Sztereotípia külf.egyetemistákkal szemben")
+
+
+# 4. abrazoljuk a manifest valtozok atlagat clusterenkent
+
+valasz_abra(lcalist = lcalist, k = 2, fajlnev = "Atlag_sztip_kEgy.png")
+
+
+
+###############################
+# LCA -- dangerous worldviews #
+###############################
+
+# 1. define PCA formula
+
+# names(df_k)
+
+# !
+i = which(names(df_k)== "danger_1")
+j = which(names(df_k)== "danger_20")
+
+manif = df_k[i:j]
+
+s = names(manif)
+s2 = paste(s, collapse=" ")
+s3 = gsub(" ", ", ", s2)
+s4 = noquote(s3)
+# s4
+
+# !
+formula = cbind(danger_1, danger_2, danger_3, danger_4, danger_5, danger_6, danger_7, danger_8, danger_9, danger_10, danger_11, danger_12, danger_13, danger_14, danger_15, danger_16, danger_17, danger_18, danger_19, danger_20
+) ~ 1
+
+
+# 2. fit LCA model
+
+# !
+n = 5 # define the number of maximum clusters the model should try
+lcalist = list()
+
+for (i in 1:n){
+  
+  set.seed(1)
+  fitlca = poLCA::poLCA(formula, data = manif, nclass = i,
+                        nrep = 3, maxiter = 5000, verbose = FALSE)
+  
+  name <- paste('fitlca',i,sep='')
+  lcalist[[name]] <- fitlca
+}
+
+
+# 3. abrazoljuk a modellek illeszkedeset
+
+bic_abra(lcalist = lcalist, n = n, 
+         fajlnev = "BIC_danger.png",
+         abracim = "Dangerous worldviews")
+
+
+# 4. abrazoljuk a manifest valtozok atlagat clusterenkent
+
+valasz_abra(lcalist = lcalist, k = 2, fajlnev = "Atlag_danger.png")
